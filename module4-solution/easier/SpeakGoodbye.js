bdaed2f3b146de8a4cbd4f3c534dd18d223d85ab
arjundhaka <149366812+arjundhaka@users.noreply.github.com>
2024-01-24 16:34:34 +0530

    Update SpeakGoodbye.js

--- module4-solution/easier/SpeakGoodbye.js
+++ module4-solution/easier/SpeakGoodbye.js
@@ -2,7 +2,7 @@
 	var byeSpeaker={};
 	var speakWord="Good Bye";
 	byeSpeaker.speak=function (name) {
-		document.write(speakWord+" "+name);
+		document.write(speakWord+" "+name+" ");
 	}
 	window.byeSpeaker=byeSpeaker;
 })(window);
