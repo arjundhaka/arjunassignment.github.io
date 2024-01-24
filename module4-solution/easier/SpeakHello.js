6377fff582071f2081abd6c6460c31f52e901c22
arjundhaka <149366812+arjundhaka@users.noreply.github.com>
2024-01-24 16:34:59 +0530

    Update SpeakHello.js

--- module4-solution/easier/SpeakHello.js
+++ module4-solution/easier/SpeakHello.js
@@ -2,7 +2,7 @@
 	var helloSpeaker={};
 	var speakWord="Hello";
 	helloSpeaker.speak=function (name) {
-		document.write(speakWord+" "+name);
+		document.write(speakWord+" "+name+" ");
 	}
 	window.helloSpeaker=helloSpeaker;
 })(window);
